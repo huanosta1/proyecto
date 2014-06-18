da5faf0b7b807e6a99a31cd820d8f4e3107efaa5
Isaac Huanosta <isaac_javier3@hotmail.com>
2014-06-18 15:37:29 -0500

    Archivo index.php modificado

---
+++ index.php
@@ -0,0 +1,35 @@
+<html>
+<body>
+<form name="mat" method="post" action="index.php">
+<input type="text" name="a" />
+<select name="opr">
+<option value="">Operador</option>
+<option value="+">Sumar</option>
+<option value="-">Restar</option>
+<option value="*">Multiplicar</option>
+<option value="/">Dividir</option>
+</select>
+<input type="text" name="b" />
+<input type="submit" name="ej" value="Ejecutar" />
+</form>
+<?php
+hola q tal:
+if(isset($_POST['ej']))
+{
+include('calculadora.php');
+
+$objeto = new calculadora();
+$objeto->a=$_REQUEST['a'];
+$objeto->b=$_REQUEST['b'];
+$objeto->opr=$_REQUEST['opr'];
+$objeto->operar();
+
+
+
+echo "Resultado= $objeto->result<br><br>";
+}
+
+?>
+
+</body>
+</html>
